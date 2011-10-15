626ea343a9c71e354a4638cf1e1ef9d9aa222f1f
moxuse <moxuse@gmail.com>
2011-10-16 02:07:16 +0900

    Revert "set link to nasa"
    This reverts commit 3bcd598d43f1d2649bc425eb4de493a55bff6f08.

--- prismEphem.py
+++ prismEphem.py
@@ -3,7 +3,6 @@
 
 import sys
 import datetime
-import pytz
 import urllib2
 import chardet
 import re
@@ -21,33 +20,23 @@ def six2ten(six):
         return t0 - t1 - t2
     
 #fetch NORAD site
-url = 'http://www.amsat.org/amsat/ftp/keps/current/nasa.all'
+url = 'http://celestrak.com/NORAD/elements/amateur.txt'
 htmlOpener = urllib2.urlopen(url)
 if(htmlOpener.code != 200): exit(0)
 src = htmlOpener.read().splitlines()
 count = 0
 for line in src:
-    if line.find('PRISM') != -1:
-        line1 = 'PRISM'
+    if line.find('PRISM    ') != -1:
+        line1 = str(count)
         line2 = src[count+1] 
         line3 = src[count+2]       
-        print line1
-        print line2 
-        print line3
+        print line1,line2,line3
     count+=1
 
-#timezone pref
-bst = pytz.timezone('Europe/London')
-timeLondon =  datetime.datetime.now(bst)
-
 #ephem convert
-# obs = ephem.Observer()
-# 
-# obs.date = timeLondon.strftime("%Y/%m/%d")
-
 iss = ephem.readtle(line1, line2, line3)
 
-timeNow = timeLondon.strftime("%Y/%m/%d %H:%M")
+timeNow = datetime.datetime.now().strftime("%Y/%m/%d %H:%M")
 
 print timeNow
 
